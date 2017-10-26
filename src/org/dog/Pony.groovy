5120bb9bbc1272c6bcfb225729de56ec466109d8
Larry Grill <larry.grill@hitachivantara.com>
2017-10-26 14:05:37 -0400

    data

--- src/org/dog/Pony.groovy
+++ src/org/dog/Pony.groovy
@@ -2,7 +2,8 @@ package org.dog
 
 public class Pony {
 
+  def ponyColors = ['pink', 'red', 'blue', 'purple']
   public def getPonyText() {
-    return 'I love pink ponies!!!'
+    return "I love ${ponyColors[1]} ponies!!!"
   }
 }
