bbde84ab8ec7f50e2120517a5a192fd4e0d572a4
Larry Grill <larry.grill@hitachivantara.com>
2018-01-04 16:31:32 -0500

    wft

--- vars/doBuild.groovy
+++ vars/doBuild.groovy
@@ -4,8 +4,9 @@ import org.dog.*
 
 def call(String dataFilePath = 'resources/builders/buildControlData.yaml') {
   echo "Loading build data from ${dataFilePath}"
-  def loader = new LoadBuildData()
-  def data = loader.LoadBuildControlData(dataFilePath)
+  
+  //def loader = new LoadBuildData()
+  //def data = loader.LoadBuildControlData(dataFilePath)
 
-  return data
+  //return data
 }
